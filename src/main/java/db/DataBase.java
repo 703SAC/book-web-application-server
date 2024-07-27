e4503e0f7b69c31ab265a40c93720c537d506a5d
CNS\LHC13 <lhc13@lgcns.com>
2024-07-27 20:11:58 +0900

    complete the rest of requirements

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -21,4 +21,8 @@ public class DataBase {
     public static Collection<User> findAll() {
         return users.values();
     }
+    public static boolean existUserId(String userId) {
+    	
+    	return users.containsKey(userId);
+    }
 }
